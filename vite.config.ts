e3b1cca940d8e72dbf570a68865f97dde7c9cd46
Mahshid Bahira <182437623+mahshidbahira@users.noreply.github.com>
2024-10-30 13:43:02 +0330

    build: fix minify identifier issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,17 @@ import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
+    minify: "esbuild",
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
       name: "TimeWise",
       fileName: "time-wise",
       formats: ["es", "cjs", "iife", "umd", "system"],
     },
+  },
+  esbuild: {
+    keepNames: true,
+    minifyIdentifiers: false,
   },
   plugins: [dts({ rollupTypes: true })],
 });

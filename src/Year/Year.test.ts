94af2ea242c9b63af7572c434146f617bbd4ca8a
Mahshid Bahira <182437623+mahshidbahira@users.noreply.github.com>
2025-10-10 19:24:58 +0330

    feat(Year): implement Year type

--- src/Year/Year.test.ts
+++ src/Year/Year.test.ts
@@ -15,7 +15,47 @@ describe("Year", () => {
       expect(year.value).toBe(2025);
     });
 
-    it("should throw an error with invalid value", () => {
+    it("should throw an error with invalid value type", () => {
+      // given
+      const value: any = "2025";
+
+      // when/then
+      expect(() => new Year(value)).toThrowError();
+    });
+
+    it("should throw an error with undefined", () => {
+      // given
+      const value: any = undefined;
+
+      // when/then
+      expect(() => new Year(value)).toThrowError();
+    });
+
+    it("should throw an error with null", () => {
+      // given
+      const value: any = null;
+
+      // when/then
+      expect(() => new Year(value)).toThrowError();
+    });
+
+    it("should throw an error with NaN", () => {
+      // given
+      const value = NaN;
+
+      // when/then
+      expect(() => new Year(value)).toThrowError();
+    });
+
+    it("should throw an error with floating point number", () => {
+      // given
+      const value = 2025.2;
+
+      // when/then
+      expect(() => new Year(value)).toThrowError();
+    });
+
+    it("should throw an error with invalid lower range value", () => {
       // given
       const value = 1968;
 
@@ -27,7 +67,7 @@ describe("Year", () => {
   describe("valueOf", () => {
     it("should return the value of the year", () => {
       // given
-      const year = new Year(2025);
+      const year = Year.fromObject({ value: 2025 });
 
       // when
       const value = year.valueOf();
@@ -40,7 +80,7 @@ describe("Year", () => {
   describe("toString", () => {
     it("should return the string of the year", () => {
       // given
-      const year = new Year(2025);
+      const year = Year.fromObject({ value: 2025 });
 
       // when
       const str = year.toString();
@@ -53,7 +93,7 @@ describe("Year", () => {
   describe("toPrimitive", () => {
     it("should return a number when a number is expected of the year", () => {
       // given
-      const year = new Year(2025);
+      const year = Year.fromObject({ value: 2025 });
 
       // when
       const primitive = +year;
@@ -64,7 +104,7 @@ describe("Year", () => {
 
     it("should return a string when a string is expected of the year", () => {
       // given
-      const year = new Year(2025);
+      const year = Year.fromObject({ value: 2025 });
 
       // when
       const primitive = `${year}`;
@@ -74,16 +114,300 @@ describe("Year", () => {
     });
   });
 
+  describe("toJSON", () => {
+    it("should return the JSON of the year", () => {
+      // given
+      const year = Year.fromObject({ value: 2025 });
+
+      // when
+      const jsonStr = JSON.stringify(year);
+
+      // then
+      expect(jsonStr).toBe(`"2025"`);
+    });
+  });
+
   describe("toISOString", () => {
     it("should return the iso string of the year", () => {
       // given
-      const year = new Year(2025);
+      const year = Year.fromObject({ value: 2025 });
 
       // when
       const isoStr = year.toISOString();
 
       // then
       expect(isoStr).toBe("2025");
+    });
+  });
+
+  describe("toObject", () => {
+    it("should return the object of the year", () => {
+      // given
+      const year = Year.fromObject({ value: 2025 });
+
+      // when
+      const objectLiteral = year.toObject();
+
+      // then
+      expect(objectLiteral).toEqual({ value: 2025 });
+    });
+  });
+
+  describe("equals", () => {
+    it("should return true for equal year objects", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2025 });
+      const year2 = Year.fromObject({ value: 2025 });
+
+      // when
+      const isEqual = year1.equals(year2);
+
+      // then
+      expect(isEqual).toBe(true);
+    });
+
+    it("should return false for unequal year objects", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2025 });
+      const year2 = Year.fromObject({ value: 2024 });
+
+      // when
+      const isEqual = year1.equals(year2);
+
+      // then
+      expect(isEqual).toBe(false);
+    });
+  });
+
+  describe("isGreaterThan", () => {
+    it("should return true when the year is greater than the other year", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2025 });
+      const year2 = Year.fromObject({ value: 2024 });
+
+      // when
+      const isGreaterThan = year1.isGreaterThan(year2);
+
+      // then
+      expect(isGreaterThan).toBe(true);
+    });
+
+    it("should return false when the year is equal to the other year", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2025 });
+      const year2 = Year.fromObject({ value: 2025 });
+
+      // when
+      const isGreaterThan = year1.isGreaterThan(year2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+
+    it("should return false when the year is less than the other year", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2024 });
+      const year2 = Year.fromObject({ value: 2025 });
+
+      // when
+      const isGreaterThan = year1.isGreaterThan(year2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+  });
+
+  describe("isLessThan", () => {
+    it("should return true when the year is less than the other year", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2024 });
+      const year2 = Year.fromObject({ value: 2025 });
+
+      // when
+      const isLessThan = year1.isLessThan(year2);
+
+      // then
+      expect(isLessThan).toBe(true);
+    });
+
+    it("should return false when the year is equal to the other year", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2025 });
+      const year2 = Year.fromObject({ value: 2025 });
+
+      // when
+      const isLessThan = year1.isLessThan(year2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+
+    it("should return false when the year is greater than the other year", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2025 });
+      const year2 = Year.fromObject({ value: 2024 });
+
+      // when
+      const isLessThan = year1.isLessThan(year2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+  });
+
+  describe("withValue", () => {
+    it("should return a year", () => {
+      // given
+      const year = Year.fromObject({ value: 2025 });
+
+      // when
+      const newYear = year.withValue(2024);
+
+      // then
+      expect(newYear).toBeInstanceOf(Year);
+      expect(newYear.value).toBe(2024);
+    });
+  });
+
+  describe("static fromObject", () => {
+    it("should return a year from an object", () => {
+      // given
+      const objectLiteral = { value: 2025 };
+
+      // when
+      const year = Year.fromObject(objectLiteral);
+
+      // then
+      expect(year).toBeInstanceOf(Year);
+      expect(year.value).toBe(2025);
+    });
+  });
+
+  describe("static fromString", () => {
+    it("should return the year from a string", () => {
+      // given
+      const str = "2025";
+
+      // when
+      const year = Year.fromString(str);
+
+      // then
+      expect(year).toBeInstanceOf(Year);
+      expect(year.value).toBe(2025);
+    });
+
+    it("should throw an error with an invalid 5-digit string", () => {
+      // given
+      const str = "20256";
+
+      // when/then
+      expect(() => Year.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "2b3h";
+
+      // when/then
+      expect(() => Year.fromString(str)).toThrowError();
+    });
+  });
+
+  describe("static fromISOString", () => {
+    it("should return the year from an iso string", () => {
+      // given
+      const str = "2025";
+
+      // when
+      const year = Year.fromISOString(str);
+
+      // then
+      expect(year).toBeInstanceOf(Year);
+      expect(year.value).toBe(2025);
+    });
+
+    it("should throw an error with an invalid 5-digit iso string", () => {
+      // given
+      const str = "20256";
+
+      // when/then
+      expect(() => Year.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric iso string", () => {
+      // given
+      const str = "2b3h";
+
+      // when/then
+      expect(() => Year.fromISOString(str)).toThrowError();
+    });
+  });
+
+  describe("static parse", () => {
+    it("should return the year from a string", () => {
+      // given
+      const str = "2025";
+
+      // when
+      const year = Year.parse(str);
+
+      // then
+      expect(year).toBeInstanceOf(Year);
+      expect(year.value).toBe(2025);
+    });
+
+    it("should throw an error with an invalid 5-digit string", () => {
+      // given
+      const str = "20256";
+
+      // when/then
+      expect(() => Year.parse(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "2b3h";
+
+      // when/then
+      expect(() => Year.parse(str)).toThrowError();
+    });
+  });
+
+  describe("static compare", () => {
+    it("should return a positive number when the year is greater than the other year", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2025 });
+      const year2 = Year.fromObject({ value: 2024 });
+
+      // when
+      const comparison = Year.compare(year1, year2);
+
+      // then
+      expect(comparison).toBe(1);
+    });
+
+    it("should return zero when the year is equal to the other year", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2025 });
+      const year2 = Year.fromObject({ value: 2025 });
+
+      // when
+      const comparison = Year.compare(year1, year2);
+
+      // then
+      expect(comparison).toBe(0);
+    });
+
+    it("should return a negative number when the year is less than the other year", () => {
+      // given
+      const year1 = Year.fromObject({ value: 2024 });
+      const year2 = Year.fromObject({ value: 2025 });
+
+      // when
+      const comparison = Year.compare(year1, year2);
+
+      // then
+      expect(comparison).toBe(-1);
     });
   });
 });
--- src/Year/Year.ts
+++ src/Year/Year.ts
@@ -1,9 +1,25 @@
+import YearObjectLiteral from "./YearObjectLiteral";
+
+const MINIMUM_YEAR: number = 1969;
+
 class Year {
   readonly value: number;
 
   constructor(value: number) {
-    if (value < 1969) {
-      throw new Error(`value is invalid: ${value}`);
+    if (typeof value !== "number") {
+      throw new Error(`year value type must be number`);
+    }
+
+    if (Number.isNaN(value)) {
+      throw new Error(`year value must be number`);
+    }
+
+    if (!Number.isInteger(value)) {
+      throw new Error(`year value must be an integer`);
+    }
+
+    if (value < MINIMUM_YEAR) {
+      throw new Error(`year value range must be more than ${MINIMUM_YEAR}`);
     }
 
     this.value = value;
@@ -30,8 +46,76 @@ class Year {
     }
   }
 
+  toJSON(): string {
+    return this.toISOString();
+  }
+
   toISOString(): string {
     return this.toString();
+  }
+
+  toObject(): YearObjectLiteral {
+    return {
+      value: this.value,
+    };
+  }
+
+  equals(other: Year): boolean {
+    return this.value === other.value;
+  }
+
+  isGreaterThan(other: Year): boolean {
+    return this.value > other.value;
+  }
+
+  isLessThan(other: Year): boolean {
+    return this.value < other.value;
+  }
+
+  withValue(value: number): Year {
+    return Year.fromObject({ value });
+  }
+
+  static fromObject(object: YearObjectLiteral): Year {
+    return new Year(object.value);
+  }
+
+  static fromString(str: string): Year {
+    const regexp = /^(\d{4})$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(
+        `year string format must be 4 consecutive digits (e.g. "2024")`
+      );
+    }
+
+    const value = parseInt(str, 10);
+
+    return Year.fromObject({ value });
+  }
+
+  static fromISOString(str: string): Year {
+    return Year.fromString(str);
+  }
+
+  static parse(str: string): Year {
+    const parsers: Array<(s: string) => Year> = [
+      Year.fromString,
+      Year.fromISOString,
+    ];
+
+    for (const parseFn of parsers) {
+      try {
+        return parseFn(str);
+      } catch {}
+    }
+
+    throw new Error(`year parse failed`);
+  }
+
+  static compare(year1: Year, year2: Year): number {
+    return year1.value - year2.value;
   }
 }
 

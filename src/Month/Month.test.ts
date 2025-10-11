24bbf525f8aa6f6b2c5f227b163ff77e40938842
Mahshid Bahira <182437623+mahshidbahira@users.noreply.github.com>
2025-10-11 16:27:01 +0330

    feat(Month): implement Month type

--- src/Month/Month.test.ts
+++ src/Month/Month.test.ts
@@ -15,7 +15,47 @@ describe("Month", () => {
       expect(month.value).toBe(12);
     });
 
-    it("should throw an error with invalid lower value", () => {
+    it("should throw an error with invalid value type", () => {
+      // given
+      const value: any = "3";
+
+      // when/then
+      expect(() => new Month(value)).toThrowError();
+    });
+
+    it("should throw an error with undefined", () => {
+      // given
+      const value: any = undefined;
+
+      // when/then
+      expect(() => new Month(value)).toThrowError();
+    });
+
+    it("should throw an error with null", () => {
+      // given
+      const value: any = null;
+
+      // when/then
+      expect(() => new Month(value)).toThrowError();
+    });
+
+    it("should throw an error with NaN", () => {
+      // given
+      const value = NaN;
+
+      // when/then
+      expect(() => new Month(value)).toThrowError();
+    });
+
+    it("should throw an error with floating point number", () => {
+      // given
+      const value = 9.2;
+
+      // when/then
+      expect(() => new Month(value)).toThrowError();
+    });
+
+    it("should throw an error with invalid lower range value", () => {
       // given
       const value = 0;
 
@@ -23,7 +63,7 @@ describe("Month", () => {
       expect(() => new Month(value)).toThrowError();
     });
 
-    it("should throw an error with invalid upper value", () => {
+    it("should throw an error with invalid upper range value", () => {
       // given
       const value = 13;
 
@@ -35,7 +75,7 @@ describe("Month", () => {
   describe("valueOf", () => {
     it("should return the value of the month", () => {
       // given
-      const month = new Month(12);
+      const month = Month.fromObject({ value: 12 });
 
       // when
       const value = month.valueOf();
@@ -46,20 +86,20 @@ describe("Month", () => {
   });
 
   describe("toString", () => {
-    it("should return the padded string of the month", () => {
+    it("should return the 1-digit string of the month", () => {
       // given
-      const month = new Month(3);
+      const month = Month.fromObject({ value: 3 });
 
       // when
       const str = month.toString();
 
       // then
-      expect(str).toBe("03");
+      expect(str).toBe("3");
     });
 
-    it("should return the string of the month", () => {
+    it("should return the 2-digit string of the month", () => {
       // given
-      const month = new Month(12);
+      const month = Month.fromObject({ value: 12 });
 
       // when
       const str = month.toString();
@@ -72,7 +112,7 @@ describe("Month", () => {
   describe("toPrimitive", () => {
     it("should return a number when a number is expected of the month", () => {
       // given
-      const month = new Month(12);
+      const month = Month.fromObject({ value: 12 });
 
       // when
       const primitive = +month;
@@ -83,7 +123,7 @@ describe("Month", () => {
 
     it("should return a string when a string is expected of the month", () => {
       // given
-      const month = new Month(12);
+      const month = Month.fromObject({ value: 12 });
 
       // when
       const primitive = `${month}`;
@@ -93,10 +133,23 @@ describe("Month", () => {
     });
   });
 
-  describe("toISOString", () => {
-    it("should return the padded iso string of the month", () => {
+  describe("toJSON", () => {
+    it("should return the JSON of the month", () => {
       // given
-      const month = new Month(3);
+      const month = Month.fromObject({ value: 3 });
+
+      // when
+      const jsonStr = JSON.stringify(month);
+
+      // then
+      expect(jsonStr).toBe(`"03"`);
+    });
+  });
+
+  describe("toISOString", () => {
+    it("should return the padded 1-digit iso string of the month", () => {
+      // given
+      const month = Month.fromObject({ value: 3 });
 
       // when
       const isoStr = month.toISOString();
@@ -105,15 +158,330 @@ describe("Month", () => {
       expect(isoStr).toBe("03");
     });
 
-    it("should return the iso string of the month", () => {
+    it("should return the 2-digit iso string of the month", () => {
       // given
-      const month = new Month(12);
+      const month = Month.fromObject({ value: 12 });
 
       // when
       const isoStr = month.toISOString();
 
       // then
       expect(isoStr).toBe("12");
+    });
+  });
+
+  describe("toObject", () => {
+    it("should return the object of the month", () => {
+      // given
+      const month = Month.fromObject({ value: 3 });
+
+      // when
+      const objectLiteral = month.toObject();
+
+      // then
+      expect(objectLiteral).toEqual({ value: 3 });
+    });
+  });
+
+  describe("equals", () => {
+    it("should return true for equal month objects", () => {
+      // given
+      const month1 = Month.fromObject({ value: 3 });
+      const month2 = Month.fromObject({ value: 3 });
+
+      // when
+      const isEqual = month1.equals(month2);
+
+      // then
+      expect(isEqual).toBe(true);
+    });
+
+    it("should return false for unequal month objects", () => {
+      // given
+      const month1 = Month.fromObject({ value: 3 });
+      const month2 = Month.fromObject({ value: 12 });
+
+      // when
+      const isEqual = month1.equals(month2);
+
+      // then
+      expect(isEqual).toBe(false);
+    });
+  });
+
+  describe("isGreaterThan", () => {
+    it("should return true when the month is greater than the other month", () => {
+      // given
+      const month1 = Month.fromObject({ value: 12 });
+      const month2 = Month.fromObject({ value: 3 });
+
+      // when
+      const isGreaterThan = month1.isGreaterThan(month2);
+
+      // then
+      expect(isGreaterThan).toBe(true);
+    });
+
+    it("should return false when the month is equal to the other month", () => {
+      // given
+      const month1 = Month.fromObject({ value: 12 });
+      const month2 = Month.fromObject({ value: 12 });
+
+      // when
+      const isGreaterThan = month1.isGreaterThan(month2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+
+    it("should return false when the month is less than the other month", () => {
+      // given
+      const month1 = Month.fromObject({ value: 3 });
+      const month2 = Month.fromObject({ value: 12 });
+
+      // when
+      const isGreaterThan = month1.isGreaterThan(month2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+  });
+
+  describe("isLessThan", () => {
+    it("should return true when the month is less than the other month", () => {
+      // given
+      const month1 = Month.fromObject({ value: 3 });
+      const month2 = Month.fromObject({ value: 12 });
+
+      // when
+      const isLessThan = month1.isLessThan(month2);
+
+      // then
+      expect(isLessThan).toBe(true);
+    });
+
+    it("should return false when the month is equal to the other month", () => {
+      // given
+      const month1 = Month.fromObject({ value: 12 });
+      const month2 = Month.fromObject({ value: 12 });
+
+      // when
+      const isLessThan = month1.isLessThan(month2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+
+    it("should return false when the month is greater than the other month", () => {
+      // given
+      const month1 = Month.fromObject({ value: 12 });
+      const month2 = Month.fromObject({ value: 3 });
+
+      // when
+      const isLessThan = month1.isLessThan(month2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+  });
+
+  describe("withValue", () => {
+    it("should return a month", () => {
+      // given
+      const month = Month.fromObject({ value: 3 });
+
+      // when
+      const newMonth = month.withValue(11);
+
+      // then
+      expect(newMonth).toBeInstanceOf(Month);
+      expect(newMonth.value).toBe(11);
+    });
+  });
+
+  describe("static fromObject", () => {
+    it("should return a month from an object", () => {
+      // given
+      const objectLiteral = { value: 12 };
+
+      // when
+      const month = Month.fromObject(objectLiteral);
+
+      // then
+      expect(month).toBeInstanceOf(Month);
+      expect(month.value).toBe(12);
+    });
+  });
+
+  describe("static fromString", () => {
+    it("should return the month from a 1-digit string", () => {
+      // given
+      const str = "3";
+
+      // when
+      const month = Month.fromString(str);
+
+      // then
+      expect(month).toBeInstanceOf(Month);
+      expect(month.value).toBe(3);
+    });
+
+    it("should return the month from a padded 1-digit string", () => {
+      // given
+      const str = "03";
+
+      // when
+      const month = Month.fromString(str);
+
+      // then
+      expect(month).toBeInstanceOf(Month);
+      expect(month.value).toBe(3);
+    });
+
+    it("should return the month from a 2-digit string", () => {
+      // given
+      const str = "12";
+
+      // when
+      const month = Month.fromString(str);
+
+      // then
+      expect(month).toBeInstanceOf(Month);
+      expect(month.value).toBe(12);
+    });
+
+    it("should throw an error with an invalid 3-digit string", () => {
+      // given
+      const str = "114";
+
+      // when/then
+      expect(() => Month.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "2b3h";
+
+      // when/then
+      expect(() => Month.fromString(str)).toThrowError();
+    });
+  });
+
+  describe("static fromISOString", () => {
+    it("should return the month from an iso string", () => {
+      // given
+      const str = "12";
+
+      // when
+      const month = Month.fromISOString(str);
+
+      // then
+      expect(month).toBeInstanceOf(Month);
+      expect(month.value).toBe(12);
+    });
+
+    it("should throw an error with an invalid 3-digit iso string", () => {
+      // given
+      const str = "114";
+
+      // when/then
+      expect(() => Month.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric iso string", () => {
+      // given
+      const str = "2b3h";
+
+      // when/then
+      expect(() => Month.fromString(str)).toThrowError();
+    });
+  });
+
+  describe("static parse", () => {
+    it("should return the month from a string", () => {
+      // given
+      const str = "3";
+
+      // when
+      const month = Month.parse(str);
+
+      // then
+      expect(month).toBeInstanceOf(Month);
+      expect(month.value).toBe(3);
+    });
+
+    it("should return the month from an iso string", () => {
+      // given
+      const str = "12";
+
+      // when
+      const month = Month.parse(str);
+
+      // then
+      expect(month).toBeInstanceOf(Month);
+      expect(month.value).toBe(12);
+    });
+
+    it("should throw an error with an invalid 3-digit string", () => {
+      // given
+      const str = "114";
+
+      // when/then
+      expect(() => Month.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "2b3h";
+
+      // when/then
+      expect(() => Month.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid empty string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Month.parse(str)).toThrowError();
+    });
+  });
+
+  describe("static compare", () => {
+    it("should return a positive number when the month is greater than the other month", () => {
+      // given
+      const month1 = Month.fromObject({ value: 12 });
+      const month2 = Month.fromObject({ value: 3 });
+
+      // when
+      const comparison = Month.compare(month1, month2);
+
+      // then
+      expect(comparison).toBe(9);
+    });
+
+    it("should return zero when the month is equal to the other month", () => {
+      // given
+      const month1 = Month.fromObject({ value: 12 });
+      const month2 = Month.fromObject({ value: 12 });
+
+      // when
+      const comparison = Month.compare(month1, month2);
+
+      // then
+      expect(comparison).toBe(0);
+    });
+
+    it("should return a negative number when the month is less than the other month", () => {
+      // given
+      const month1 = Month.fromObject({ value: 3 });
+      const month2 = Month.fromObject({ value: 12 });
+
+      // when
+      const comparison = Month.compare(month1, month2);
+
+      // then
+      expect(comparison).toBe(-9);
     });
   });
 });
--- src/Month/Month.ts
+++ src/Month/Month.ts
@@ -1,9 +1,28 @@
+import MonthObjectLiteral from "./MonthObjectLiteral";
+
+const MINIMUM_MONTH: number = 1;
+const MAXIMUM_MONTH: number = 12;
+
 class Month {
   readonly value: number;
 
   constructor(value: number) {
-    if (value < 1 || value > 12) {
-      throw new Error(`value is invalid: ${value}`);
+    if (typeof value !== "number") {
+      throw new Error(`month value type must be number`);
+    }
+
+    if (Number.isNaN(value)) {
+      throw new Error(`month value must be number`);
+    }
+
+    if (!Number.isInteger(value)) {
+      throw new Error(`month value must be an integer`);
+    }
+
+    if (value < MINIMUM_MONTH || value > MAXIMUM_MONTH) {
+      throw new Error(
+        `month value range must be between ${MINIMUM_MONTH} and ${MAXIMUM_MONTH}`
+      );
     }
 
     this.value = value;
@@ -16,7 +35,7 @@ class Month {
   }
 
   toString(): string {
-    return this.value.toString().padStart(2, "0");
+    return this.value.toString();
   }
 
   [Symbol.toPrimitive](hint: string): number | string {
@@ -30,8 +49,85 @@ class Month {
     }
   }
 
+  toJSON(): string {
+    return this.toISOString();
+  }
+
   toISOString(): string {
-    return this.toString();
+    return this.value.toString().padStart(2, "0");
+  }
+
+  toObject(): MonthObjectLiteral {
+    return {
+      value: this.value,
+    };
+  }
+
+  equals(other: Month): boolean {
+    return this.value === other.value;
+  }
+
+  isGreaterThan(other: Month): boolean {
+    return this.value > other.value;
+  }
+
+  isLessThan(other: Month): boolean {
+    return this.value < other.value;
+  }
+
+  withValue(value: number): Month {
+    return Month.fromObject({ value });
+  }
+
+  static fromObject(object: MonthObjectLiteral): Month {
+    return new Month(object.value);
+  }
+
+  static fromString(str: string): Month {
+    const regexp = /^\d{1,2}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(
+        `month string format must be 1 digit or 2 consecutive digits`
+      );
+    }
+
+    const value = parseInt(str, 10);
+
+    return Month.fromObject({ value });
+  }
+
+  static fromISOString(str: string): Month {
+    const regexp = /^\d{2}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(`month string format must be 2 consecutive digits`);
+    }
+
+    const value = parseInt(str, 10);
+
+    return Month.fromObject({ value });
+  }
+
+  static parse(str: string): Month {
+    const parsers: Array<(s: string) => Month> = [
+      Month.fromString,
+      Month.fromISOString,
+    ];
+
+    for (const parseFn of parsers) {
+      try {
+        return parseFn(str);
+      } catch {}
+    }
+
+    throw new Error(`month parse failed`);
+  }
+
+  static compare(month1: Month, month2: Month): number {
+    return month1.value - month2.value;
   }
 }
 

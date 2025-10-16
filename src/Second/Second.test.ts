01f999430ec54f87892b58e5e7e83202387e5c86
Mahshid Bahira <182437623+mahshidbahira@users.noreply.github.com>
2025-10-16 15:52:39 +0000

    feat(Second): add Second type

--- src/Second/Second.test.ts
+++ src/Second/Second.test.ts
@@ -15,7 +15,47 @@ describe("Second", () => {
       expect(second.value).toBe(45);
     });
 
-    it("should throw an error with invalid lower value", () => {
+    it("should throw an error with invalid value type", () => {
+      // given
+      const value: any = "59"; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Second(value)).toThrowError();
+    });
+
+    it("should throw an error with undefined", () => {
+      // given
+      const value: any = undefined; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Second(value)).toThrowError();
+    });
+
+    it("should throw an error with null", () => {
+      // given
+      const value: any = null; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Second(value)).toThrowError();
+    });
+
+    it("should throw an error with NaN", () => {
+      // given
+      const value = NaN;
+
+      // when/then
+      expect(() => new Second(value)).toThrowError();
+    });
+
+    it("should throw an error with floating point number", () => {
+      // given
+      const value = 45.2;
+
+      // when/then
+      expect(() => new Second(value)).toThrowError();
+    });
+
+    it("should throw an error with invalid lower range value", () => {
       // given
       const value = -1;
 
@@ -23,7 +63,7 @@ describe("Second", () => {
       expect(() => new Second(value)).toThrowError();
     });
 
-    it("should throw an error with invalid upper value", () => {
+    it("should throw an error with invalid upper range value", () => {
       // given
       const value = 60;
 
@@ -35,7 +75,7 @@ describe("Second", () => {
   describe("valueOf", () => {
     it("should return the value of the second", () => {
       // given
-      const second = new Second(45);
+      const second = Second.fromObject({ value: 45 });
 
       // when
       const value = second.valueOf();
@@ -48,18 +88,18 @@ describe("Second", () => {
   describe("toString", () => {
     it("should return the padded string of the second", () => {
       // given
-      const second = new Second(9);
+      const second = Second.fromObject({ value: 9 });
 
       // when
       const str = second.toString();
 
       // then
-      expect(str).toBe("09");
+      expect(str).toBe("9");
     });
 
     it("should return the string of the second", () => {
       // given
-      const second = new Second(45);
+      const second = Second.fromObject({ value: 45 });
 
       // when
       const str = second.toString();
@@ -72,7 +112,7 @@ describe("Second", () => {
   describe("toPrimitive", () => {
     it("should return a number when a number is expected of the second", () => {
       // given
-      const second = new Second(45);
+      const second = Second.fromObject({ value: 45 });
 
       // when
       const primitive = +second;
@@ -83,7 +123,7 @@ describe("Second", () => {
 
     it("should return a string when a string is expected of the second", () => {
       // given
-      const second = new Second(45);
+      const second = Second.fromObject({ value: 45 });
 
       // when
       const primitive = `${second}`;
@@ -93,10 +133,23 @@ describe("Second", () => {
     });
   });
 
+  describe("toJSON", () => {
+    it("should return the JSON of the second", () => {
+      // given
+      const second = Second.fromObject({ value: 9 });
+
+      // when
+      const jsonStr = JSON.stringify(second);
+
+      // then
+      expect(jsonStr).toBe(`"09"`);
+    });
+  });
+
   describe("toISOString", () => {
     it("should return the padded iso string of the second", () => {
       // given
-      const second = new Second(9);
+      const second = Second.fromObject({ value: 9 });
 
       // when
       const isoStr = second.toISOString();
@@ -107,13 +160,364 @@ describe("Second", () => {
 
     it("should return the iso string of the second", () => {
       // given
-      const second = new Second(45);
+      const second = Second.fromObject({ value: 45 });
 
       // when
       const isoStr = second.toISOString();
 
       // then
       expect(isoStr).toBe("45");
+    });
+  });
+
+  describe("toObject", () => {
+    it("should return the object of the second", () => {
+      // given
+      const second = Second.fromObject({ value: 9 });
+
+      // when
+      const objectLiteral = second.toObject();
+
+      // then
+      expect(objectLiteral).toEqual({ value: 9 });
+    });
+  });
+
+  describe("equals", () => {
+    it("should return true for equal second objects", () => {
+      // given
+      const second1 = Second.fromObject({ value: 9 });
+      const second2 = Second.fromObject({ value: 9 });
+
+      // when
+      const isEqual = second1.equals(second2);
+
+      // then
+      expect(isEqual).toBe(true);
+    });
+
+    it("should return false for unequal second objects", () => {
+      // given
+      const second1 = Second.fromObject({ value: 9 });
+      const second2 = Second.fromObject({ value: 45 });
+
+      // when
+      const isEqual = second1.equals(second2);
+
+      // then
+      expect(isEqual).toBe(false);
+    });
+  });
+
+  describe("isGreaterThan", () => {
+    it("should return true when the second is greater than the other second", () => {
+      // given
+      const second1 = Second.fromObject({ value: 45 });
+      const second2 = Second.fromObject({ value: 9 });
+
+      // when
+      const isGreaterThan = second1.isGreaterThan(second2);
+
+      // then
+      expect(isGreaterThan).toBe(true);
+    });
+
+    it("should return false when the second is equal to the other second", () => {
+      // given
+      const second1 = Second.fromObject({ value: 45 });
+      const second2 = Second.fromObject({ value: 45 });
+
+      // when
+      const isGreaterThan = second1.isGreaterThan(second2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+
+    it("should return false when the second is less than the other second", () => {
+      // given
+      const second1 = Second.fromObject({ value: 9 });
+      const second2 = Second.fromObject({ value: 45 });
+
+      // when
+      const isGreaterThan = second1.isGreaterThan(second2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+  });
+
+  describe("isLessThan", () => {
+    it("should return true when the second is less than the other second", () => {
+      // given
+      const second1 = Second.fromObject({ value: 9 });
+      const second2 = Second.fromObject({ value: 45 });
+
+      // when
+      const isLessThan = second1.isLessThan(second2);
+
+      // then
+      expect(isLessThan).toBe(true);
+    });
+
+    it("should return false when the second is equal to the other second", () => {
+      // given
+      const second1 = Second.fromObject({ value: 45 });
+      const second2 = Second.fromObject({ value: 45 });
+
+      // when
+      const isLessThan = second1.isLessThan(second2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+
+    it("should return false when the second is greater than the other second", () => {
+      // given
+      const second1 = Second.fromObject({ value: 45 });
+      const second2 = Second.fromObject({ value: 9 });
+
+      // when
+      const isLessThan = second1.isLessThan(second2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+  });
+
+  describe("withValue", () => {
+    it("should return a second", () => {
+      // given
+      const second = Second.fromObject({ value: 9 });
+
+      // when
+      const newSecond = second.withValue(45);
+
+      // then
+      expect(newSecond).toBeInstanceOf(Second);
+      expect(newSecond.value).toBe(45);
+    });
+  });
+
+  describe("static fromObject", () => {
+    it("should return a second from an object", () => {
+      // given
+      const objectLiteral = { value: 45 };
+
+      // when
+      const second = Second.fromObject(objectLiteral);
+
+      // then
+      expect(second).toBeInstanceOf(Second);
+      expect(second.value).toBe(45);
+    });
+  });
+
+  describe("static fromString", () => {
+    it("should return the second from a 1-digit string", () => {
+      // given
+      const str = "9";
+
+      // when
+      const second = Second.fromString(str);
+
+      // then
+      expect(second).toBeInstanceOf(Second);
+      expect(second.value).toBe(9);
+    });
+
+    it("should return the second from a 2-digit string", () => {
+      // given
+      const str = "45";
+
+      // when
+      const second = Second.fromString(str);
+
+      // then
+      expect(second).toBeInstanceOf(Second);
+      expect(second.value).toBe(45);
+    });
+
+    it("should return the second from a padded 1-digit string", () => {
+      // given
+      const str = "09";
+
+      // when
+      const second = Second.fromString(str);
+
+      // then
+      expect(second).toBeInstanceOf(Second);
+      expect(second.value).toBe(9);
+    });
+
+    it("should throw an error with an invalid empty string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Second.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 3-digit string", () => {
+      // given
+      const str = "592";
+
+      // when/then
+      expect(() => Second.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "5b";
+
+      // when/then
+      expect(() => Second.fromString(str)).toThrowError();
+    });
+  });
+
+  describe("static fromISOString", () => {
+    it("should return the second from a padded 1-digit iso string", () => {
+      // given
+      const str = "04";
+
+      // when
+      const second = Second.fromISOString(str);
+
+      // then
+      expect(second).toBeInstanceOf(Second);
+      expect(second.value).toBe(4);
+    });
+
+    it("should return the second from a 2-digit iso string", () => {
+      // given
+      const str = "45";
+
+      // when
+      const second = Second.fromISOString(str);
+
+      // then
+      expect(second).toBeInstanceOf(Second);
+      expect(second.value).toBe(45);
+    });
+
+    it("should throw an error with an invalid empty iso string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Second.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 1-digit iso string", () => {
+      // given
+      const str = "4";
+
+      // when/then
+      expect(() => Second.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 3-digit iso string", () => {
+      // given
+      const str = "592";
+
+      // when/then
+      expect(() => Second.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric iso string", () => {
+      // given
+      const str = "5b";
+
+      // when/then
+      expect(() => Second.fromISOString(str)).toThrowError();
+    });
+  });
+
+  describe("static parse", () => {
+    it("should return the second from a string", () => {
+      // given
+      const str = "9";
+
+      // when
+      const second = Second.parse(str);
+
+      // then
+      expect(second).toBeInstanceOf(Second);
+      expect(second.value).toBe(9);
+    });
+
+    it("should return the second from an iso string", () => {
+      // given
+      const str = "45";
+
+      // when
+      const second = Second.parse(str);
+
+      // then
+      expect(second).toBeInstanceOf(Second);
+      expect(second.value).toBe(45);
+    });
+
+    it("should throw an error with an invalid empty string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Second.parse(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 3-digit string", () => {
+      // given
+      const str = "592";
+
+      // when/then
+      expect(() => Second.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "5b";
+
+      // when/then
+      expect(() => Second.fromString(str)).toThrowError();
+    });
+  });
+
+  describe("static compare", () => {
+    it("should return a positive number when the second is greater than the other second", () => {
+      // given
+      const second1 = Second.fromObject({ value: 45 });
+      const second2 = Second.fromObject({ value: 9 });
+
+      // when
+      const comparison = Second.compare(second1, second2);
+
+      // then
+      expect(comparison).toBe(36);
+    });
+
+    it("should return zero when the second is equal to the other second", () => {
+      // given
+      const second1 = Second.fromObject({ value: 45 });
+      const second2 = Second.fromObject({ value: 45 });
+
+      // when
+      const comparison = Second.compare(second1, second2);
+
+      // then
+      expect(comparison).toBe(0);
+    });
+
+    it("should return a negative number when the second is less than the other second", () => {
+      // given
+      const second1 = Second.fromObject({ value: 9 });
+      const second2 = Second.fromObject({ value: 45 });
+
+      // when
+      const comparison = Second.compare(second1, second2);
+
+      // then
+      expect(comparison).toBe(-36);
     });
   });
 });
--- src/Second/Second.ts
+++ src/Second/Second.ts
@@ -1,9 +1,28 @@
+import SecondObjectLiteral from "./SecondObjectLiteral";
+
+const MINIMUM_SECOND: number = 0;
+const MAXIMUM_SECOND: number = 59;
+
 class Second {
   readonly value: number;
 
   constructor(value: number) {
-    if (value < 0 || value > 59) {
-      throw new Error(`value is invalid: ${value}`);
+    if (typeof value !== "number") {
+      throw new Error(`second value type must be number`);
+    }
+
+    if (Number.isNaN(value)) {
+      throw new Error(`second value must be number`);
+    }
+
+    if (!Number.isInteger(value)) {
+      throw new Error(`second value must be an integer`);
+    }
+
+    if (value < MINIMUM_SECOND || value > MAXIMUM_SECOND) {
+      throw new Error(
+        `second value range must be between ${MINIMUM_SECOND} and ${MAXIMUM_SECOND}`
+      );
     }
 
     this.value = value;
@@ -16,7 +35,7 @@ class Second {
   }
 
   toString(): string {
-    return this.value.toString().padStart(2, "0");
+    return this.value.toString();
   }
 
   [Symbol.toPrimitive](hint: string): number | string {
@@ -30,8 +49,85 @@ class Second {
     }
   }
 
+  toJSON(): string {
+    return this.toISOString();
+  }
+
   toISOString(): string {
-    return this.toString();
+    return this.value.toString().padStart(2, "0");
+  }
+
+  toObject(): SecondObjectLiteral {
+    return {
+      value: this.value,
+    };
+  }
+
+  equals(other: Second): boolean {
+    return this.value === other.value;
+  }
+
+  isGreaterThan(other: Second): boolean {
+    return this.value > other.value;
+  }
+
+  isLessThan(other: Second): boolean {
+    return this.value < other.value;
+  }
+
+  withValue(value: number): Second {
+    return Second.fromObject({ value });
+  }
+
+  static fromObject(object: SecondObjectLiteral): Second {
+    return new Second(object.value);
+  }
+
+  static fromString(str: string): Second {
+    const regexp = /^\d{1,2}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(
+        `second string format must be 1 digit or 2 consecutive digits`
+      );
+    }
+
+    const value = parseInt(str, 10);
+
+    return Second.fromObject({ value });
+  }
+
+  static fromISOString(str: string): Second {
+    const regexp = /^\d{2}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(`second iso string format must be 2 consecutive digits`);
+    }
+
+    const value = parseInt(str, 10);
+
+    return Second.fromObject({ value });
+  }
+
+  static parse(str: string): Second {
+    const parsers: Array<(s: string) => Second> = [
+      Second.fromString,
+      Second.fromISOString,
+    ];
+
+    for (const parseFn of parsers) {
+      try {
+        return parseFn(str);
+      } catch {} // eslint-disable-line no-empty
+    }
+
+    throw new Error(`second parse failed`);
+  }
+
+  static compare(second1: Second, second2: Second): number {
+    return second1.value - second2.value;
   }
 }
 

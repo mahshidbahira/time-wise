fa4b4e559e1b01d9e8392426d79e913fdd36c800
Mahshid Bahira <182437623+mahshidbahira@users.noreply.github.com>
2025-10-16 15:22:12 +0000

    fix(Year): fix `fromISOString` error message

--- src/Year/Year.ts
+++ src/Year/Year.ts
@@ -98,7 +98,7 @@ class Year {
     const result = regexp.test(str);
 
     if (!result) {
-      throw new Error(`year string format must be 4 consecutive digits`);
+      throw new Error(`year iso string format must be 4 consecutive digits`);
     }
 
     const value = parseInt(str, 10);

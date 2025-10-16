4b2ca8e3218394a81acca6821c84fe0a26f670bd
Mahshid Bahira <182437623+mahshidbahira@users.noreply.github.com>
2025-10-16 15:26:14 +0000

    feat(Hour): implement Hour type

--- src/Hour/Hour.test.ts
+++ src/Hour/Hour.test.ts
@@ -15,7 +15,47 @@ describe("Hour", () => {
       expect(hour.value).toBe(12);
     });
 
-    it("should throw an error with invalid lower value", () => {
+    it("should throw an error with invalid value type", () => {
+      // given
+      const value: any = "23"; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Hour(value)).toThrowError();
+    });
+
+    it("should throw an error with undefined", () => {
+      // given
+      const value: any = undefined; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Hour(value)).toThrowError();
+    });
+
+    it("should throw an error with null", () => {
+      // given
+      const value: any = null; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Hour(value)).toThrowError();
+    });
+
+    it("should throw an error with NaN", () => {
+      // given
+      const value = NaN;
+
+      // when/then
+      expect(() => new Hour(value)).toThrowError();
+    });
+
+    it("should throw an error with floating point number", () => {
+      // given
+      const value = 12.2;
+
+      // when/then
+      expect(() => new Hour(value)).toThrowError();
+    });
+
+    it("should throw an error with invalid lower range value", () => {
       // given
       const value = -1;
 
@@ -23,7 +63,7 @@ describe("Hour", () => {
       expect(() => new Hour(value)).toThrowError();
     });
 
-    it("should throw an error with invalid upper value", () => {
+    it("should throw an error with invalid upper range value", () => {
       // given
       const value = 24;
 
@@ -35,7 +75,7 @@ describe("Hour", () => {
   describe("valueOf", () => {
     it("should return the value of the hour", () => {
       // given
-      const hour = new Hour(12);
+      const hour = Hour.fromObject({ value: 12 });
 
       // when
       const value = hour.valueOf();
@@ -46,20 +86,20 @@ describe("Hour", () => {
   });
 
   describe("toString", () => {
-    it("should return the padded string of the hour", () => {
+    it("should return the 1-digit string of the hour", () => {
       // given
-      const hour = new Hour(9);
+      const hour = Hour.fromObject({ value: 9 });
 
       // when
       const str = hour.toString();
 
       // then
-      expect(str).toBe("09");
+      expect(str).toBe("9");
     });
 
-    it("should return the string of the hour", () => {
+    it("should return the 2-digit string of the hour", () => {
       // given
-      const hour = new Hour(12);
+      const hour = Hour.fromObject({ value: 12 });
 
       // when
       const str = hour.toString();
@@ -72,7 +112,7 @@ describe("Hour", () => {
   describe("toPrimitive", () => {
     it("should return a number when a number is expected of the hour", () => {
       // given
-      const hour = new Hour(12);
+      const hour = Hour.fromObject({ value: 12 });
 
       // when
       const primitive = +hour;
@@ -83,7 +123,7 @@ describe("Hour", () => {
 
     it("should return a string when a string is expected of the hour", () => {
       // given
-      const hour = new Hour(12);
+      const hour = Hour.fromObject({ value: 12 });
 
       // when
       const primitive = `${hour}`;
@@ -93,10 +133,23 @@ describe("Hour", () => {
     });
   });
 
-  describe("toISOString", () => {
-    it("should return the padded iso string of a hour", () => {
+  describe("toJSON", () => {
+    it("should return the JSON of the hour", () => {
       // given
-      const hour = new Hour(9);
+      const hour = Hour.fromObject({ value: 9 });
+
+      // when
+      const jsonStr = JSON.stringify(hour);
+
+      // then
+      expect(jsonStr).toBe(`"09"`);
+    });
+  });
+
+  describe("toISOString", () => {
+    it("should return the padded 1-digit iso string of the hour", () => {
+      // given
+      const hour = Hour.fromObject({ value: 9 });
 
       // when
       const isoStr = hour.toISOString();
@@ -105,15 +158,366 @@ describe("Hour", () => {
       expect(isoStr).toBe("09");
     });
 
-    it("should return the iso string of a hour", () => {
+    it("should return the 2-digit iso string of an hour", () => {
       // given
-      const hour = new Hour(12);
+      const hour = Hour.fromObject({ value: 12 });
 
       // when
       const isoStr = hour.toISOString();
 
       // then
       expect(isoStr).toBe("12");
+    });
+  });
+
+  describe("toObject", () => {
+    it("should return the object of the hour", () => {
+      // given
+      const hour = Hour.fromObject({ value: 9 });
+
+      // when
+      const objectLiteral = hour.toObject();
+
+      // then
+      expect(objectLiteral).toEqual({ value: 9 });
+    });
+  });
+
+  describe("equals", () => {
+    it("should return true for equal hour objects", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 9 });
+      const hour2 = Hour.fromObject({ value: 9 });
+
+      // when
+      const isEqual = hour1.equals(hour2);
+
+      // then
+      expect(isEqual).toBe(true);
+    });
+
+    it("should return false for unequal hour objects", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 9 });
+      const hour2 = Hour.fromObject({ value: 12 });
+
+      // when
+      const isEqual = hour1.equals(hour2);
+
+      // then
+      expect(isEqual).toBe(false);
+    });
+  });
+
+  describe("isGreaterThan", () => {
+    it("should return true when the hour is greater than the other hour", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 12 });
+      const hour2 = Hour.fromObject({ value: 9 });
+
+      // when
+      const isGreaterThan = hour1.isGreaterThan(hour2);
+
+      // then
+      expect(isGreaterThan).toBe(true);
+    });
+
+    it("should return false when the hour is equal to the other hour", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 12 });
+      const hour2 = Hour.fromObject({ value: 12 });
+
+      // when
+      const isGreaterThan = hour1.isGreaterThan(hour2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+
+    it("should return false when the hour is less than the other hour", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 9 });
+      const hour2 = Hour.fromObject({ value: 12 });
+
+      // when
+      const isGreaterThan = hour1.isGreaterThan(hour2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+  });
+
+  describe("isLessThan", () => {
+    it("should return true when the hour is less than the other hour", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 9 });
+      const hour2 = Hour.fromObject({ value: 12 });
+
+      // when
+      const isLessThan = hour1.isLessThan(hour2);
+
+      // then
+      expect(isLessThan).toBe(true);
+    });
+
+    it("should return false when the hour is equal to the other hour", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 12 });
+      const hour2 = Hour.fromObject({ value: 12 });
+
+      // when
+      const isLessThan = hour1.isLessThan(hour2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+
+    it("should return false when the hour is greater than the other hour", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 12 });
+      const hour2 = Hour.fromObject({ value: 9 });
+
+      // when
+      const isLessThan = hour1.isLessThan(hour2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+  });
+
+  describe("withValue", () => {
+    it("should return an hour", () => {
+      // given
+      const hour = Hour.fromObject({ value: 9 });
+
+      // when
+      const newHour = hour.withValue(12);
+
+      // then
+      expect(newHour).toBeInstanceOf(Hour);
+      expect(newHour.value).toBe(12);
+    });
+  });
+
+  describe("static fromObject", () => {
+    it("should return an hour from an object", () => {
+      // given
+      const objectLiteral = { value: 12 };
+
+      // when
+      const hour = Hour.fromObject(objectLiteral);
+
+      // then
+      expect(hour).toBeInstanceOf(Hour);
+      expect(hour.value).toBe(12);
+    });
+  });
+
+  describe("static fromString", () => {
+    it("should return the hour from a 1-digit string", () => {
+      // given
+      const str = "9";
+
+      // when
+      const hour = Hour.fromString(str);
+
+      // then
+      expect(hour).toBeInstanceOf(Hour);
+      expect(hour.value).toBe(9);
+    });
+
+    it("should return the hour from a 2-digit string", () => {
+      // given
+      const str = "12";
+
+      // when
+      const hour = Hour.fromString(str);
+
+      // then
+      expect(hour).toBeInstanceOf(Hour);
+      expect(hour.value).toBe(12);
+    });
+
+    it("should return the hour from a padded 1-digit string", () => {
+      // given
+      const str = "09";
+
+      // when
+      const hour = Hour.fromString(str);
+
+      // then
+      expect(hour).toBeInstanceOf(Hour);
+      expect(hour.value).toBe(9);
+    });
+
+    it("should throw an error with an invalid empty string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Hour.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 3-digit string", () => {
+      // given
+      const str = "202";
+
+      // when/then
+      expect(() => Hour.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "2b";
+
+      // when/then
+      expect(() => Hour.fromString(str)).toThrowError();
+    });
+  });
+
+  describe("static fromISOString", () => {
+    it("should return the hour from a padded 1-digit iso string", () => {
+      // given
+      const str = "09";
+
+      // when
+      const hour = Hour.fromISOString(str);
+
+      // then
+      expect(hour).toBeInstanceOf(Hour);
+      expect(hour.value).toBe(9);
+    });
+
+    it("should return the hour from a 2-digit iso string", () => {
+      // given
+      const str = "12";
+
+      // when
+      const hour = Hour.fromISOString(str);
+
+      // then
+      expect(hour).toBeInstanceOf(Hour);
+      expect(hour.value).toBe(12);
+    });
+
+    it("should throw an error with an invalid empty iso string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Hour.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 1-digit iso string", () => {
+      // given
+      const str = "9";
+
+      // when/then
+      expect(() => Hour.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 3-digit iso string", () => {
+      // given
+      const str = "202";
+
+      // when/then
+      expect(() => Hour.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric iso string", () => {
+      // given
+      const str = "2b";
+
+      // when/then
+      expect(() => Hour.fromISOString(str)).toThrowError();
+    });
+  });
+
+  describe("static parse", () => {
+    it("should return the hour from a string", () => {
+      // given
+      const str = "9";
+
+      // when
+      const hour = Hour.parse(str);
+
+      // then
+      expect(hour).toBeInstanceOf(Hour);
+      expect(hour.value).toBe(9);
+    });
+
+    it("should return the hour from an iso string", () => {
+      // given
+      const str = "12";
+
+      // when
+      const hour = Hour.parse(str);
+
+      // then
+      expect(hour).toBeInstanceOf(Hour);
+      expect(hour.value).toBe(12);
+    });
+
+    it("should throw an error with an invalid empty string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Hour.parse(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 3-digit string", () => {
+      // given
+      const str = "202";
+
+      // when/then
+      expect(() => Hour.parse(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "2b";
+
+      // when/then
+      expect(() => Hour.parse(str)).toThrowError();
+    });
+  });
+
+  describe("static compare", () => {
+    it("should return a positive number when the hour is greater than the other hour", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 12 });
+      const hour2 = Hour.fromObject({ value: 9 });
+
+      // when
+      const comparison = Hour.compare(hour1, hour2);
+
+      // then
+      expect(comparison).toBe(3);
+    });
+
+    it("should return zero when the hour is equal to the other hour", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 12 });
+      const hour2 = Hour.fromObject({ value: 12 });
+
+      // when
+      const comparison = Hour.compare(hour1, hour2);
+
+      // then
+      expect(comparison).toBe(0);
+    });
+
+    it("should return a negative number when the hour is less than the other hour", () => {
+      // given
+      const hour1 = Hour.fromObject({ value: 9 });
+      const hour2 = Hour.fromObject({ value: 12 });
+
+      // when
+      const comparison = Hour.compare(hour1, hour2);
+
+      // then
+      expect(comparison).toBe(-3);
     });
   });
 });
--- src/Hour/Hour.ts
+++ src/Hour/Hour.ts
@@ -1,9 +1,28 @@
+import HourObjectLiteral from "./HourObjectLiteral";
+
+const MINIMUM_HOUR: number = 0;
+const MAXIMUM_HOUR: number = 23;
+
 class Hour {
   readonly value: number;
 
   constructor(value: number) {
-    if (value < 0 || value > 23) {
-      throw new Error(`value is invalid: ${value}`);
+    if (typeof value !== "number") {
+      throw new Error(`hour value type must be number`);
+    }
+
+    if (Number.isNaN(value)) {
+      throw new Error(`hour value must be number`);
+    }
+
+    if (!Number.isInteger(value)) {
+      throw new Error(`hour value must be an integer`);
+    }
+
+    if (value < MINIMUM_HOUR || value > MAXIMUM_HOUR) {
+      throw new Error(
+        `hour value range must be between ${MINIMUM_HOUR} and ${MAXIMUM_HOUR}`
+      );
     }
 
     this.value = value;
@@ -16,7 +35,7 @@ class Hour {
   }
 
   toString(): string {
-    return this.value.toString().padStart(2, "0");
+    return this.value.toString();
   }
 
   [Symbol.toPrimitive](hint: string): number | string {
@@ -30,8 +49,85 @@ class Hour {
     }
   }
 
+  toJSON(): string {
+    return this.toISOString();
+  }
+
   toISOString(): string {
-    return this.toString();
+    return this.value.toString().padStart(2, "0");
+  }
+
+  toObject(): HourObjectLiteral {
+    return {
+      value: this.value,
+    };
+  }
+
+  equals(other: Hour): boolean {
+    return this.value === other.value;
+  }
+
+  isGreaterThan(other: Hour): boolean {
+    return this.value > other.value;
+  }
+
+  isLessThan(other: Hour): boolean {
+    return this.value < other.value;
+  }
+
+  withValue(value: number): Hour {
+    return Hour.fromObject({ value });
+  }
+
+  static fromObject(object: HourObjectLiteral): Hour {
+    return new Hour(object.value);
+  }
+
+  static fromString(str: string): Hour {
+    const regexp = /^\d{1,2}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(
+        `hour string format must be 1 digit or 2 consecutive digits`
+      );
+    }
+
+    const value = parseInt(str, 10);
+
+    return Hour.fromObject({ value });
+  }
+
+  static fromISOString(str: string): Hour {
+    const regexp = /^\d{2}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(`hour iso string format must be 2 consecutive digits`);
+    }
+
+    const value = parseInt(str, 10);
+
+    return Hour.fromObject({ value });
+  }
+
+  static parse(str: string): Hour {
+    const parsers: Array<(s: string) => Hour> = [
+      Hour.fromString,
+      Hour.fromISOString,
+    ];
+
+    for (const parseFn of parsers) {
+      try {
+        return parseFn(str);
+      } catch {} // eslint-disable-line no-empty
+    }
+
+    throw new Error(`hour parse failed`);
+  }
+
+  static compare(hour1: Hour, hour2: Hour): number {
+    return hour1.value - hour2.value;
   }
 }
 

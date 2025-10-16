618dfcee7490d76be4b561fb848e7694073fd54f
Mahshid Bahira <182437623+mahshidbahira@users.noreply.github.com>
2025-10-16 16:04:28 +0000

    feat(Millisecond): add Millisecond type

--- src/Millisecond/Millisecond.test.ts
+++ src/Millisecond/Millisecond.test.ts
@@ -15,7 +15,47 @@ describe("Millisecond", () => {
       expect(millisecond.value).toBe(999);
     });
 
-    it("should throw an error with invalid lower value", () => {
+    it("should throw an error with invalid value type", () => {
+      // given
+      const value: any = "999"; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Millisecond(value)).toThrowError();
+    });
+
+    it("should throw an error with undefined", () => {
+      // given
+      const value: any = undefined; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Millisecond(value)).toThrowError();
+    });
+
+    it("should throw an error with null", () => {
+      // given
+      const value: any = null; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Millisecond(value)).toThrowError();
+    });
+
+    it("should throw an error with NaN", () => {
+      // given
+      const value = NaN;
+
+      // when/then
+      expect(() => new Millisecond(value)).toThrowError();
+    });
+
+    it("should throw an error with floating point number", () => {
+      // given
+      const value = 2.2;
+
+      // when/then
+      expect(() => new Millisecond(value)).toThrowError();
+    });
+
+    it("should throw an error with invalid lower range", () => {
       // given
       const value = -1;
 
@@ -23,7 +63,7 @@ describe("Millisecond", () => {
       expect(() => new Millisecond(value)).toThrowError();
     });
 
-    it("should throw an error with invalid upper value", () => {
+    it("should throw an error with invalid upper range", () => {
       // given
       const value = 1000;
 
@@ -35,7 +75,7 @@ describe("Millisecond", () => {
   describe("valueOf", () => {
     it("should return the value of the millisecond", () => {
       // given
-      const millisecond = new Millisecond(999);
+      const millisecond = Millisecond.fromObject({ value: 999 });
 
       // when
       const value = millisecond.valueOf();
@@ -46,20 +86,31 @@ describe("Millisecond", () => {
   });
 
   describe("toString", () => {
-    it("should return the padded string of the millisecond", () => {
+    it("should return the 1-digit string of the millisecond", () => {
       // given
-      const millisecond = new Millisecond(2);
+      const millisecond = Millisecond.fromObject({ value: 2 });
 
       // when
       const str = millisecond.toString();
 
       // then
-      expect(str).toBe("002");
+      expect(str).toBe("2");
     });
 
-    it("should return the string of the millisecond", () => {
+    it("should return the 2-digit string of the millisecond", () => {
       // given
-      const millisecond = new Millisecond(999);
+      const millisecond = Millisecond.fromObject({ value: 99 });
+
+      // when
+      const str = millisecond.toString();
+
+      // then
+      expect(str).toBe("99");
+    });
+
+    it("should return the 3-digit string of the millisecond", () => {
+      // given
+      const millisecond = Millisecond.fromObject({ value: 999 });
 
       // when
       const str = millisecond.toString();
@@ -72,7 +123,7 @@ describe("Millisecond", () => {
   describe("toPrimitive", () => {
     it("should return a number when a number is expected of the millisecond", () => {
       // given
-      const millisecond = new Millisecond(999);
+      const millisecond = Millisecond.fromObject({ value: 999 });
 
       // when
       const primitive = +millisecond;
@@ -83,7 +134,7 @@ describe("Millisecond", () => {
 
     it("should return a string when a string is expected of the millisecond", () => {
       // given
-      const millisecond = new Millisecond(999);
+      const millisecond = Millisecond.fromObject({ value: 999 });
 
       // when
       const primitive = `${millisecond}`;
@@ -93,10 +144,23 @@ describe("Millisecond", () => {
     });
   });
 
-  describe("toISOString", () => {
-    it("should return the padded iso string of the millisecond", () => {
+  describe("toJSON", () => {
+    it("should return the JSON of the millisecond", () => {
       // given
-      const millisecond = new Millisecond(2);
+      const millisecond = Millisecond.fromObject({ value: 2 });
+
+      // when
+      const jsonStr = JSON.stringify(millisecond);
+
+      // then
+      expect(jsonStr).toBe(`"002"`);
+    });
+  });
+
+  describe("toISOString", () => {
+    it("should return the padded 1-digit iso string of the millisecond", () => {
+      // given
+      const millisecond = Millisecond.fromObject({ value: 2 });
 
       // when
       const isoStr = millisecond.toISOString();
@@ -105,15 +169,421 @@ describe("Millisecond", () => {
       expect(isoStr).toBe("002");
     });
 
-    it("should return the iso string of the millisecond", () => {
+    it("should return the padded 2-digit iso string of the millisecond", () => {
       // given
-      const millisecond = new Millisecond(999);
+      const millisecond = Millisecond.fromObject({ value: 99 });
+
+      // when
+      const isoStr = millisecond.toISOString();
+
+      // then
+      expect(isoStr).toBe("099");
+    });
+
+    it("should return the 3-digit iso string of the millisecond", () => {
+      // given
+      const millisecond = Millisecond.fromObject({ value: 999 });
 
       // when
       const isoStr = millisecond.toISOString();
 
       // then
       expect(isoStr).toBe("999");
+    });
+  });
+
+  describe("toObject", () => {
+    it("should return the object of the millisecond", () => {
+      // given
+      const millisecond = Millisecond.fromObject({ value: 2 });
+
+      // when
+      const objectLiteral = millisecond.toObject();
+
+      // then
+      expect(objectLiteral).toEqual({ value: 2 });
+    });
+  });
+
+  describe("equals", () => {
+    it("should return true for equal millisecond objects", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 2 });
+      const second2 = Millisecond.fromObject({ value: 2 });
+
+      // when
+      const isEqual = second1.equals(second2);
+
+      // then
+      expect(isEqual).toBe(true);
+    });
+
+    it("should return false for unequal millisecond objects", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 2 });
+      const second2 = Millisecond.fromObject({ value: 999 });
+
+      // when
+      const isEqual = second1.equals(second2);
+
+      // then
+      expect(isEqual).toBe(false);
+    });
+  });
+
+  describe("isGreaterThan", () => {
+    it("should return true when the millisecond is greater than the other millisecond", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 999 });
+      const second2 = Millisecond.fromObject({ value: 2 });
+
+      // when
+      const isGreaterThan = second1.isGreaterThan(second2);
+
+      // then
+      expect(isGreaterThan).toBe(true);
+    });
+
+    it("should return false when the millisecond is equal to the other millisecond", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 999 });
+      const second2 = Millisecond.fromObject({ value: 999 });
+
+      // when
+      const isGreaterThan = second1.isGreaterThan(second2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+
+    it("should return false when the millisecond is less than the other millisecond", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 2 });
+      const second2 = Millisecond.fromObject({ value: 999 });
+
+      // when
+      const isGreaterThan = second1.isGreaterThan(second2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+  });
+
+  describe("isLessThan", () => {
+    it("should return true when the millisecond is less than the other millisecond", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 2 });
+      const second2 = Millisecond.fromObject({ value: 999 });
+
+      // when
+      const isLessThan = second1.isLessThan(second2);
+
+      // then
+      expect(isLessThan).toBe(true);
+    });
+
+    it("should return false when the millisecond is equal to the other millisecond", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 999 });
+      const second2 = Millisecond.fromObject({ value: 999 });
+
+      // when
+      const isLessThan = second1.isLessThan(second2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+
+    it("should return false when the millisecond is greater than the other millisecond", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 999 });
+      const second2 = Millisecond.fromObject({ value: 2 });
+
+      // when
+      const isLessThan = second1.isLessThan(second2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+  });
+
+  describe("withValue", () => {
+    it("should return a millisecond", () => {
+      // given
+      const millisecond = Millisecond.fromObject({ value: 2 });
+
+      // when
+      const newSecond = millisecond.withValue(999);
+
+      // then
+      expect(newSecond).toBeInstanceOf(Millisecond);
+      expect(newSecond.value).toBe(999);
+    });
+  });
+
+  describe("static fromObject", () => {
+    it("should return a millisecond from an object", () => {
+      // given
+      const objectLiteral = { value: 999 };
+
+      // when
+      const millisecond = Millisecond.fromObject(objectLiteral);
+
+      // then
+      expect(millisecond).toBeInstanceOf(Millisecond);
+      expect(millisecond.value).toBe(999);
+    });
+  });
+
+  describe("static fromString", () => {
+    it("should return the millisecond from a 1-digit string", () => {
+      // given
+      const str = "2";
+
+      // when
+      const second = Millisecond.fromString(str);
+
+      // then
+      expect(second).toBeInstanceOf(Millisecond);
+      expect(second.value).toBe(2);
+    });
+
+    it("should return the millisecond from a 2-digit string", () => {
+      // given
+      const str = "99";
+
+      // when
+      const second = Millisecond.fromString(str);
+
+      // then
+      expect(second).toBeInstanceOf(Millisecond);
+      expect(second.value).toBe(99);
+    });
+
+    it("should return the millisecond from a 3-digit string", () => {
+      // given
+      const str = "999";
+
+      // when
+      const millisecond = Millisecond.fromString(str);
+
+      // then
+      expect(millisecond).toBeInstanceOf(Millisecond);
+      expect(millisecond.value).toBe(999);
+    });
+
+    it("should return the millisecond from a padded 1-digit string", () => {
+      // given
+      const str = "002";
+
+      // when
+      const second = Millisecond.fromString(str);
+
+      // then
+      expect(second).toBeInstanceOf(Millisecond);
+      expect(second.value).toBe(2);
+    });
+
+    it("should return the millisecond from a padded 2-digit string", () => {
+      // given
+      const str = "099";
+
+      // when
+      const second = Millisecond.fromString(str);
+
+      // then
+      expect(second).toBeInstanceOf(Millisecond);
+      expect(second.value).toBe(99);
+    });
+
+    it("should throw an error with an invalid empty string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Millisecond.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 4-digit string", () => {
+      // given
+      const str = "9979";
+
+      // when/then
+      expect(() => Millisecond.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "99b";
+
+      // when/then
+      expect(() => Millisecond.fromString(str)).toThrowError();
+    });
+  });
+
+  describe("static fromISOString", () => {
+    it("should return the millisecond from a padded 1-digit iso string", () => {
+      // given
+      const str = "002";
+
+      // when
+      const millisecond = Millisecond.fromISOString(str);
+
+      // then
+      expect(millisecond).toBeInstanceOf(Millisecond);
+      expect(millisecond.value).toBe(2);
+    });
+
+    it("should return the millisecond from a padded 2-digit iso string", () => {
+      // given
+      const str = "099";
+
+      // when
+      const millisecond = Millisecond.fromISOString(str);
+
+      // then
+      expect(millisecond).toBeInstanceOf(Millisecond);
+      expect(millisecond.value).toBe(99);
+    });
+
+    it("should return the millisecond from a 3-digit iso string", () => {
+      // given
+      const str = "999";
+
+      // when
+      const millisecond = Millisecond.fromISOString(str);
+
+      // then
+      expect(millisecond).toBeInstanceOf(Millisecond);
+      expect(millisecond.value).toBe(999);
+    });
+
+    it("should throw an error with an invalid empty iso string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Millisecond.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 1-digit iso string", () => {
+      // given
+      const str = "2";
+
+      // when/then
+      expect(() => Millisecond.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 2-digit iso string", () => {
+      // given
+      const str = "99";
+
+      // when/then
+      expect(() => Millisecond.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 4-digit iso string", () => {
+      // given
+      const str = "9979";
+
+      // when/then
+      expect(() => Millisecond.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric iso string", () => {
+      // given
+      const str = "99b";
+
+      // when/then
+      expect(() => Millisecond.fromISOString(str)).toThrowError();
+    });
+  });
+
+  describe("static parse", () => {
+    it("should return the millisecond from a string", () => {
+      // given
+      const str = "2";
+
+      // when
+      const millisecond = Millisecond.parse(str);
+
+      // then
+      expect(millisecond).toBeInstanceOf(Millisecond);
+      expect(millisecond.value).toBe(2);
+    });
+
+    it("should return the millisecond from an iso string", () => {
+      // given
+      const str = "999";
+
+      // when
+      const millisecond = Millisecond.parse(str);
+
+      // then
+      expect(millisecond).toBeInstanceOf(Millisecond);
+      expect(millisecond.value).toBe(999);
+    });
+
+    it("should throw an error with an invalid empty string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Millisecond.parse(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 4-digit string", () => {
+      // given
+      const str = "9979";
+
+      // when/then
+      expect(() => Millisecond.parse(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "99b";
+
+      // when/then
+      expect(() => Millisecond.parse(str)).toThrowError();
+    });
+  });
+
+  describe("static compare", () => {
+    it("should return a positive number when the millisecond is greater than the other millisecond", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 999 });
+      const second2 = Millisecond.fromObject({ value: 2 });
+
+      // when
+      const comparison = Millisecond.compare(second1, second2);
+
+      // then
+      expect(comparison).toBe(997);
+    });
+
+    it("should return zero when the millisecond is equal to the other millisecond", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 999 });
+      const second2 = Millisecond.fromObject({ value: 999 });
+
+      // when
+      const comparison = Millisecond.compare(second1, second2);
+
+      // then
+      expect(comparison).toBe(0);
+    });
+
+    it("should return a negative number when the millisecond is less than the other millisecond", () => {
+      // given
+      const second1 = Millisecond.fromObject({ value: 2 });
+      const second2 = Millisecond.fromObject({ value: 999 });
+
+      // when
+      const comparison = Millisecond.compare(second1, second2);
+
+      // then
+      expect(comparison).toBe(-997);
     });
   });
 });
--- src/Millisecond/Millisecond.ts
+++ src/Millisecond/Millisecond.ts
@@ -1,9 +1,28 @@
+import MillisecondObjectLiteral from "./MillisecondObjectLiteral";
+
+const MINIMUM_MILLISECOND: number = 0;
+const MAXIMUM_MILLISECOND: number = 999;
+
 class Millisecond {
   readonly value: number;
 
   constructor(value: number) {
-    if (value < 0 || value > 999) {
-      throw new Error(`value is invalid: ${value}`);
+    if (typeof value !== "number") {
+      throw new Error(`millisecond value type must be number`);
+    }
+
+    if (Number.isNaN(value)) {
+      throw new Error(`millisecond value must be number`);
+    }
+
+    if (!Number.isInteger(value)) {
+      throw new Error(`millisecond value must be an integer`);
+    }
+
+    if (value < MINIMUM_MILLISECOND || value > MAXIMUM_MILLISECOND) {
+      throw new Error(
+        `millisecond value range must be between ${MINIMUM_MILLISECOND} and ${MAXIMUM_MILLISECOND}`
+      );
     }
 
     this.value = value;
@@ -16,7 +35,7 @@ class Millisecond {
   }
 
   toString(): string {
-    return this.value.toString().padStart(3, "0");
+    return this.value.toString();
   }
 
   [Symbol.toPrimitive](hint: string): number | string {
@@ -30,8 +49,87 @@ class Millisecond {
     }
   }
 
+  toJSON(): string {
+    return this.toISOString();
+  }
+
   toISOString(): string {
-    return this.toString();
+    return this.value.toString().padStart(3, "0");
+  }
+
+  toObject(): MillisecondObjectLiteral {
+    return {
+      value: this.value,
+    };
+  }
+
+  equals(other: Millisecond): boolean {
+    return this.value === other.value;
+  }
+
+  isGreaterThan(other: Millisecond): boolean {
+    return this.value > other.value;
+  }
+
+  isLessThan(other: Millisecond): boolean {
+    return this.value < other.value;
+  }
+
+  withValue(value: number): Millisecond {
+    return Millisecond.fromObject({ value });
+  }
+
+  static fromObject(object: MillisecondObjectLiteral): Millisecond {
+    return new Millisecond(object.value);
+  }
+
+  static fromString(str: string): Millisecond {
+    const regexp = /^\d{1,3}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(
+        `millisecond string format must be 1 digit or 2 or 3 consecutive digits`
+      );
+    }
+
+    const value = parseInt(str, 10);
+
+    return Millisecond.fromObject({ value });
+  }
+
+  static fromISOString(str: string): Millisecond {
+    const regexp = /^\d{3}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(
+        `millisecond iso string format must be 3 consecutive digits`
+      );
+    }
+
+    const value = parseInt(str, 10);
+
+    return Millisecond.fromObject({ value });
+  }
+
+  static parse(str: string): Millisecond {
+    const parsers: Array<(s: string) => Millisecond> = [
+      Millisecond.fromString,
+      Millisecond.fromISOString,
+    ];
+
+    for (const parseFn of parsers) {
+      try {
+        return parseFn(str);
+      } catch {} // eslint-disable-line no-empty
+    }
+
+    throw new Error(`millisecond parse failed`);
+  }
+
+  static compare(millisecond1: Millisecond, millisecond2: Millisecond): number {
+    return millisecond1.value - millisecond2.value;
   }
 }
 

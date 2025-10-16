6e9afda11f04def2225c5b7894a240bebbf72b9f
Mahshid Bahira <182437623+mahshidbahira@users.noreply.github.com>
2025-10-16 15:34:16 +0000

    feat(Minute): add Minute type

--- src/Minute/Minute.test.ts
+++ src/Minute/Minute.test.ts
@@ -15,7 +15,47 @@ describe("Minute", () => {
       expect(minute.value).toBe(30);
     });
 
-    it("should throw an error with invalid lower value", () => {
+    it("should throw an error with invalid value type", () => {
+      // given
+      const value: any = "23"; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Minute(value)).toThrowError();
+    });
+
+    it("should throw an error with undefined", () => {
+      // given
+      const value: any = undefined; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Minute(value)).toThrowError();
+    });
+
+    it("should throw an error with null", () => {
+      // given
+      const value: any = null; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+      // when/then
+      expect(() => new Minute(value)).toThrowError();
+    });
+
+    it("should throw an error with NaN", () => {
+      // given
+      const value = NaN;
+
+      // when/then
+      expect(() => new Minute(value)).toThrowError();
+    });
+
+    it("should throw an error with floating point number", () => {
+      // given
+      const value = 30.2;
+
+      // when/then
+      expect(() => new Minute(value)).toThrowError();
+    });
+
+    it("should throw an error with invalid lower range value", () => {
       // given
       const value = -1;
 
@@ -23,7 +63,7 @@ describe("Minute", () => {
       expect(() => new Minute(value)).toThrowError();
     });
 
-    it("should throw an error with invalid upper value", () => {
+    it("should throw an error with invalid upper range value", () => {
       // given
       const value = 60;
 
@@ -35,7 +75,7 @@ describe("Minute", () => {
   describe("valueOf", () => {
     it("should return the value of the minute", () => {
       // given
-      const minute = new Minute(30);
+      const minute = Minute.fromObject({ value: 30 });
 
       // when
       const value = minute.valueOf();
@@ -46,20 +86,20 @@ describe("Minute", () => {
   });
 
   describe("toString", () => {
-    it("should return the padded string of the minute", () => {
+    it("should return the 1-digit string of the minute", () => {
       // given
-      const minute = new Minute(3);
+      const minute = Minute.fromObject({ value: 3 });
 
       // when
       const str = minute.toString();
 
       // then
-      expect(str).toBe("03");
+      expect(str).toBe("3");
     });
 
-    it("should return the string of the minute", () => {
+    it("should return the 2-digit string of the minute", () => {
       // given
-      const minute = new Minute(30);
+      const minute = Minute.fromObject({ value: 30 });
 
       // when
       const str = minute.toString();
@@ -72,7 +112,7 @@ describe("Minute", () => {
   describe("toPrimitive", () => {
     it("should return a number when a number is expected of the minute", () => {
       // given
-      const minute = new Minute(30);
+      const minute = Minute.fromObject({ value: 30 });
 
       // when
       const primitive = +minute;
@@ -83,7 +123,7 @@ describe("Minute", () => {
 
     it("should return a string when a string is expected of the minute", () => {
       // given
-      const minute = new Minute(30);
+      const minute = Minute.fromObject({ value: 30 });
 
       // when
       const primitive = `${minute}`;
@@ -93,10 +133,23 @@ describe("Minute", () => {
     });
   });
 
-  describe("toISOString", () => {
-    it("should return the padded iso string of the minute", () => {
+  describe("toJSON", () => {
+    it("should return the JSON of the minute", () => {
       // given
-      const minute = new Minute(3);
+      const minute = Minute.fromObject({ value: 3 });
+
+      // when
+      const jsonStr = JSON.stringify(minute);
+
+      // then
+      expect(jsonStr).toBe(`"03"`);
+    });
+  });
+
+  describe("toISOString", () => {
+    it("should return the padded 1-digit iso string of the minute", () => {
+      // given
+      const minute = Minute.fromObject({ value: 3 });
 
       // when
       const isoStr = minute.toISOString();
@@ -105,15 +158,366 @@ describe("Minute", () => {
       expect(isoStr).toBe("03");
     });
 
-    it("should return the iso string of the minute", () => {
+    it("should return the 2-digit iso string of the minute", () => {
       // given
-      const minute = new Minute(30);
+      const minute = Minute.fromObject({ value: 30 });
 
       // when
       const isoStr = minute.toISOString();
 
       // then
       expect(isoStr).toBe("30");
+    });
+  });
+
+  describe("toObject", () => {
+    it("should return the object of the minute", () => {
+      // given
+      const minute = Minute.fromObject({ value: 3 });
+
+      // when
+      const objectLiteral = minute.toObject();
+
+      // then
+      expect(objectLiteral).toEqual({ value: 3 });
+    });
+  });
+
+  describe("equals", () => {
+    it("should return true for equal minute objects", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 3 });
+      const minute2 = Minute.fromObject({ value: 3 });
+
+      // when
+      const isEqual = minute1.equals(minute2);
+
+      // then
+      expect(isEqual).toBe(true);
+    });
+
+    it("should return false for unequal minute objects", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 3 });
+      const minute2 = Minute.fromObject({ value: 30 });
+
+      // when
+      const isEqual = minute1.equals(minute2);
+
+      // then
+      expect(isEqual).toBe(false);
+    });
+  });
+
+  describe("isGreaterThan", () => {
+    it("should return true when the minute is greater than the other minute", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 30 });
+      const minute2 = Minute.fromObject({ value: 3 });
+
+      // when
+      const isGreaterThan = minute1.isGreaterThan(minute2);
+
+      // then
+      expect(isGreaterThan).toBe(true);
+    });
+
+    it("should return false when the minute is equal to the other minute", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 30 });
+      const minute2 = Minute.fromObject({ value: 30 });
+
+      // when
+      const isGreaterThan = minute1.isGreaterThan(minute2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+
+    it("should return false when the minute is less than the other minute", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 3 });
+      const minute2 = Minute.fromObject({ value: 30 });
+
+      // when
+      const isGreaterThan = minute1.isGreaterThan(minute2);
+
+      // then
+      expect(isGreaterThan).toBe(false);
+    });
+  });
+
+  describe("isLessThan", () => {
+    it("should return true when the minute is less than the other minute", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 3 });
+      const minute2 = Minute.fromObject({ value: 30 });
+
+      // when
+      const isLessThan = minute1.isLessThan(minute2);
+
+      // then
+      expect(isLessThan).toBe(true);
+    });
+
+    it("should return false when the minute is equal to the other minute", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 30 });
+      const minute2 = Minute.fromObject({ value: 30 });
+
+      // when
+      const isLessThan = minute1.isLessThan(minute2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+
+    it("should return false when the minute is greater than the other minute", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 30 });
+      const minute2 = Minute.fromObject({ value: 3 });
+
+      // when
+      const isLessThan = minute1.isLessThan(minute2);
+
+      // then
+      expect(isLessThan).toBe(false);
+    });
+  });
+
+  describe("withValue", () => {
+    it("should return a minute", () => {
+      // given
+      const minute = Minute.fromObject({ value: 3 });
+
+      // when
+      const newMinute = minute.withValue(30);
+
+      // then
+      expect(newMinute).toBeInstanceOf(Minute);
+      expect(newMinute.value).toBe(30);
+    });
+  });
+
+  describe("static fromObject", () => {
+    it("should return a minute from an object", () => {
+      // given
+      const objectLiteral = { value: 30 };
+
+      // when
+      const minute = Minute.fromObject(objectLiteral);
+
+      // then
+      expect(minute).toBeInstanceOf(Minute);
+      expect(minute.value).toBe(30);
+    });
+  });
+
+  describe("static fromString", () => {
+    it("should return the minute from 1-digit string", () => {
+      // given
+      const str = "3";
+
+      // when
+      const minute = Minute.fromString(str);
+
+      // then
+      expect(minute).toBeInstanceOf(Minute);
+      expect(minute.value).toBe(3);
+    });
+
+    it("should return the minute from a 2-digit string", () => {
+      // given
+      const str = "30";
+
+      // when
+      const minute = Minute.fromString(str);
+
+      // then
+      expect(minute).toBeInstanceOf(Minute);
+      expect(minute.value).toBe(30);
+    });
+
+    it("should return the minute from a padded 1-digit string", () => {
+      // given
+      const str = "03";
+
+      // when
+      const minute = Minute.fromString(str);
+
+      // then
+      expect(minute).toBeInstanceOf(Minute);
+      expect(minute.value).toBe(3);
+    });
+
+    it("should throw an error with an invalid empty string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Minute.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 3-digit string", () => {
+      // given
+      const str = "592";
+
+      // when/then
+      expect(() => Minute.fromString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "5b";
+
+      // when/then
+      expect(() => Minute.fromString(str)).toThrowError();
+    });
+  });
+
+  describe("static fromISOString", () => {
+    it("should return the minute from a padded 1-digit iso string", () => {
+      // given
+      const str = "03";
+
+      // when
+      const minute = Minute.fromISOString(str);
+
+      // then
+      expect(minute).toBeInstanceOf(Minute);
+      expect(minute.value).toBe(3);
+    });
+
+    it("should return the minute from a 2-digit iso string", () => {
+      // given
+      const str = "30";
+
+      // when
+      const minute = Minute.fromISOString(str);
+
+      // then
+      expect(minute).toBeInstanceOf(Minute);
+      expect(minute.value).toBe(30);
+    });
+
+    it("should throw an error with an invalid empty iso string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Minute.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 1-digit iso string", () => {
+      // given
+      const str = "3";
+
+      // when/then
+      expect(() => Minute.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 3-digit iso string", () => {
+      // given
+      const str = "592";
+
+      // when/then
+      expect(() => Minute.fromISOString(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric iso string", () => {
+      // given
+      const str = "5b";
+
+      // when/then
+      expect(() => Minute.fromISOString(str)).toThrowError();
+    });
+  });
+
+  describe("static parse", () => {
+    it("should return the minute from a string", () => {
+      // given
+      const str = "3";
+
+      // when
+      const minute = Minute.parse(str);
+
+      // then
+      expect(minute).toBeInstanceOf(Minute);
+      expect(minute.value).toBe(3);
+    });
+
+    it("should return the minute from an iso string", () => {
+      // given
+      const str = "30";
+
+      // when
+      const minute = Minute.parse(str);
+
+      // then
+      expect(minute).toBeInstanceOf(Minute);
+      expect(minute.value).toBe(30);
+    });
+
+    it("should throw an error with an invalid empty string", () => {
+      // given
+      const str = "";
+
+      // when/then
+      expect(() => Minute.parse(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid 3-digit string", () => {
+      // given
+      const str = "592";
+
+      // when/then
+      expect(() => Minute.parse(str)).toThrowError();
+    });
+
+    it("should throw an error with an invalid alphanumeric string", () => {
+      // given
+      const str = "5b";
+
+      // when/then
+      expect(() => Minute.parse(str)).toThrowError();
+    });
+  });
+
+  describe("static compare", () => {
+    it("should return a positive number when the minute is greater than the other minute", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 30 });
+      const minute2 = Minute.fromObject({ value: 3 });
+
+      // when
+      const comparison = Minute.compare(minute1, minute2);
+
+      // then
+      expect(comparison).toBe(27);
+    });
+
+    it("should return zero when the minute is equal to the other minute", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 30 });
+      const minute2 = Minute.fromObject({ value: 30 });
+
+      // when
+      const comparison = Minute.compare(minute1, minute2);
+
+      // then
+      expect(comparison).toBe(0);
+    });
+
+    it("should return a negative number when the minute is less than the other minute", () => {
+      // given
+      const minute1 = Minute.fromObject({ value: 3 });
+      const minute2 = Minute.fromObject({ value: 30 });
+
+      // when
+      const comparison = Minute.compare(minute1, minute2);
+
+      // then
+      expect(comparison).toBe(-27);
     });
   });
 });
--- src/Minute/Minute.ts
+++ src/Minute/Minute.ts
@@ -1,9 +1,28 @@
+import MinuteObjectLiteral from "./MinuteObjectLiteral";
+
+const MINIMUM_MINUTE: number = 0;
+const MAXIMUM_MINUTE: number = 59;
+
 class Minute {
   readonly value: number;
 
   constructor(value: number) {
-    if (value < 0 || value > 59) {
-      throw new Error(`value is invalid: ${value}`);
+    if (typeof value !== "number") {
+      throw new Error(`minute value type must be number`);
+    }
+
+    if (Number.isNaN(value)) {
+      throw new Error(`minute value must be number`);
+    }
+
+    if (!Number.isInteger(value)) {
+      throw new Error(`minute value must be an integer`);
+    }
+
+    if (value < MINIMUM_MINUTE || value > MAXIMUM_MINUTE) {
+      throw new Error(
+        `minute value range must be between ${MINIMUM_MINUTE} and ${MAXIMUM_MINUTE}`
+      );
     }
 
     this.value = value;
@@ -16,7 +35,7 @@ class Minute {
   }
 
   toString(): string {
-    return this.value.toString().padStart(2, "0");
+    return this.value.toString();
   }
 
   [Symbol.toPrimitive](hint: string): number | string {
@@ -30,8 +49,85 @@ class Minute {
     }
   }
 
+  toJSON(): string {
+    return this.toISOString();
+  }
+
   toISOString(): string {
-    return this.toString();
+    return this.value.toString().padStart(2, "0");
+  }
+
+  toObject(): MinuteObjectLiteral {
+    return {
+      value: this.value,
+    };
+  }
+
+  equals(other: Minute): boolean {
+    return this.value === other.value;
+  }
+
+  isGreaterThan(other: Minute): boolean {
+    return this.value > other.value;
+  }
+
+  isLessThan(other: Minute): boolean {
+    return this.value < other.value;
+  }
+
+  withValue(value: number): Minute {
+    return Minute.fromObject({ value });
+  }
+
+  static fromObject(object: MinuteObjectLiteral): Minute {
+    return new Minute(object.value);
+  }
+
+  static fromString(str: string): Minute {
+    const regexp = /^\d{1,2}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(
+        `minute string format must be 1 digit or 2 consecutive digits`
+      );
+    }
+
+    const value = parseInt(str, 10);
+
+    return Minute.fromObject({ value });
+  }
+
+  static fromISOString(str: string): Minute {
+    const regexp = /^\d{2}$/;
+    const result = regexp.test(str);
+
+    if (!result) {
+      throw new Error(`minute iso string format must be 2 consecutive digits`);
+    }
+
+    const value = parseInt(str, 10);
+
+    return Minute.fromObject({ value });
+  }
+
+  static parse(str: string): Minute {
+    const parsers: Array<(s: string) => Minute> = [
+      Minute.fromString,
+      Minute.fromISOString,
+    ];
+
+    for (const parseFn of parsers) {
+      try {
+        return parseFn(str);
+      } catch {} // eslint-disable-line no-empty
+    }
+
+    throw new Error(`minute parse failed`);
+  }
+
+  static compare(minute1: Minute, minute2: Minute): number {
+    return minute1.value - minute2.value;
   }
 }
 
